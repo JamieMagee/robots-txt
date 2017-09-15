1f303d15425639370262be9175b7e7129da568cb
JamieMagee <jamie.magee@gmail.com>
2017-09-15 22:03:34 +0200

    Correctly keep track of prefixes

--- scraper/middlewares.py
+++ scraper/middlewares.py
@@ -1,18 +1,30 @@
-from scrapy.downloadermiddlewares.retry import RetryMiddleware
 from random import choice
 
-from twisted.internet.error import DNSLookupError
+from scrapy.downloadermiddlewares.retry import RetryMiddleware
 
 COMMON_PREFIXES = {'http://', 'https://', 'http://www.', 'https://www.'}
 
 
 class PrefixRetryMiddleware(RetryMiddleware):
-
     def process_exception(self, request, exception, spider):
         prefixes_tried = request.meta['prefixes']
         if COMMON_PREFIXES == prefixes_tried:
             return exception
-        domain = request.meta['domain']
+
         new_prefix = choice(tuple(COMMON_PREFIXES - prefixes_tried))
-        request = request.replace(url=new_prefix + domain + '/robots.txt')
+        request = self.add_prefix(request, new_prefix)
+
         return self._retry(request, exception, spider)
+
+    @staticmethod
+    def add_prefix(request, prefix):
+        domain = request.meta['domain']
+        meta = request.meta.copy()
+        meta['prefixes'].add(prefix)
+
+        request = request.replace(
+            url=prefix + domain + '/robots.txt',
+            meta=meta
+        )
+
+        return request
